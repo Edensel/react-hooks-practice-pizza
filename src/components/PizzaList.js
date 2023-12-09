9731251735808d274f7c4b5e2ca2490e27887c8d
DenselEsekon <densel.esekon@moringaschool.com>
2023-12-09 13:26:34 +0300

    Rendered the list of pizzas in the table

--- src/components/PizzaList.js
+++ src/components/PizzaList.js
@@ -1,7 +1,26 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Pizza from "./Pizza";
 
 function PizzaList() {
+  const [pizzas, setPizzas] = useState([]);
+
+  useEffect(() => {
+    // Fetch the data from the server when the component mounts
+    fetch("http://localhost:3001/pizzas")
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        setPizzas(data);
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+      });
+  }, []); // Empty dependency array to fetch data only once on mount
+
   return (
     <table className="table table-striped">
       <thead>
@@ -13,9 +32,9 @@ function PizzaList() {
         </tr>
       </thead>
       <tbody>
-        {
-          //render Pizza here
-        }
+        {pizzas.map((pizza) => (
+          <Pizza key={pizza.id} pizza={pizza} />
+        ))}
       </tbody>
     </table>
   );
